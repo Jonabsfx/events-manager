86c7e3587db0cbedb640a74b04a7fa25de6f23ad
Jonab <jonabsf@gmail.com>
2022-06-07 05:52:39 -0300

    Config Mongo and Event model

---
+++ Models/Event.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+
+export default mongoose.model('Event', eventSchema);
--- app.js
+++ app.js
@@ -2,37 +2,83 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
+import mongoose from 'mongoose';
+import Event from './Models/Event.js';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
-        schema: buildSchema(`
-            type RootQuery {
-                events: [String!]!
-            }
-
-            type RootMutation {
-                createEvent(name: String): String
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-        rootValue: {
-            events: () => {
-                return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
-            },
-            createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
-            }
-        },
-        graphiql: true
-    })
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
+    },
+    graphiql: true
+  })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+   `mongodb+srv://jonabsfx:XwLsbt7KfaxjCoUZ@database.m0skrmk.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
